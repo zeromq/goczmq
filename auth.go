1a242b4891fd68481a7b51e0ea21333de419d9e5
Brian Knox <bknox@digitalocean.com>
2014-10-19 09:54:48 -0400

    PLAIN auth working

--- auth.go
+++ auth.go
@@ -27,51 +27,67 @@ func NewAuth() *Auth {
 }
 
 // Verbose sets the auth actor to log information to stdout.
-func (z *Auth) Verbose() error {
-	rc := C.zstr_send(unsafe.Pointer(z.zactor_t), C.CString("VERBOSE"))
+func (a *Auth) Verbose() error {
+	rc := C.zstr_send(unsafe.Pointer(a.zactor_t), C.CString("VERBOSE"))
 	if rc == -1 {
 		return ErrActorCmd
 	}
-	C.zsock_wait(unsafe.Pointer(z.zactor_t))
+	C.zsock_wait(unsafe.Pointer(a.zactor_t))
 
 	return nil
 }
 
 // Deny adds an address to a socket's deny list
-func (z *Auth) Deny(address string) error {
-	rc := C.zstr_sendm(unsafe.Pointer(z.zactor_t), C.CString("DENY"))
+func (a *Auth) Deny(address string) error {
+	rc := C.zstr_sendm(unsafe.Pointer(a.zactor_t), C.CString("DENY"))
 	if rc == -1 {
 		return ErrActorCmd
 	}
 
-	rc = C.zstr_send(unsafe.Pointer(z.zactor_t), C.CString(address))
+	rc = C.zstr_send(unsafe.Pointer(a.zactor_t), C.CString(address))
 	if rc == -1 {
 		return ErrActorCmd
 	}
 
-	C.zsock_wait(unsafe.Pointer(z.zactor_t))
+	C.zsock_wait(unsafe.Pointer(a.zactor_t))
 
 	return nil
 }
 
 // Allow removes a previous Deny
-func (z *Auth) Allow(address string) error {
-	rc := C.zstr_sendm(unsafe.Pointer(z.zactor_t), C.CString("ALLOW"))
+func (a *Auth) Allow(address string) error {
+	rc := C.zstr_sendm(unsafe.Pointer(a.zactor_t), C.CString("ALLOW"))
 	if rc == -1 {
 		return ErrActorCmd
 	}
 
-	rc = C.zstr_send(unsafe.Pointer(z.zactor_t), C.CString(address))
+	rc = C.zstr_send(unsafe.Pointer(a.zactor_t), C.CString(address))
 	if rc == -1 {
 		return ErrActorCmd
 	}
 
-	C.zsock_wait(unsafe.Pointer(z.zactor_t))
+	C.zsock_wait(unsafe.Pointer(a.zactor_t))
+
+	return nil
+}
+
+func (a *Auth) Plain(directory string) error {
+	rc := C.zstr_sendm(unsafe.Pointer(a.zactor_t), C.CString("PLAIN"))
+	if rc == -1 {
+		return ErrActorCmd
+	}
+
+	rc = C.zstr_send(unsafe.Pointer(a.zactor_t), C.CString(directory))
+	if rc == -1 {
+		return ErrActorCmd
+	}
+
+	C.zsock_wait(unsafe.Pointer(a.zactor_t))
 
 	return nil
 }
 
 // Destroy destroys the auth actor.
-func (z *Auth) Destroy() {
-	C.zactor_destroy(&z.zactor_t)
+func (a *Auth) Destroy() {
+	C.zactor_destroy(&a.zactor_t)
 }
--- auth_test.go
+++ auth_test.go
@@ -1,7 +1,9 @@
 package goczmq
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"testing"
 )
 
@@ -57,47 +59,142 @@ func TestAuthNoDeny(t *testing.T) {
 	}
 }
 
-func TestAuthDeny(t *testing.T) {
-	server := NewSock(PULL)
-	defer server.Destroy()
-
-	server.SetZapDomain("global")
-	server.Bind("tcp://*:9999")
-
-	client := NewSock(PUSH)
-	defer client.Destroy()
-
+func TestAuthPlainDeny(t *testing.T) {
+	// create an auth service
 	auth := NewAuth()
 	defer auth.Destroy()
 
+	// set verbose
 	err := auth.Verbose()
 	if err != nil {
 		t.Errorf("VERBOSE error: %s", err)
 	}
 
-	auth.Deny("127.0.0.1")
+	// allow localhost connections
+	err = auth.Allow("127.0.0.1")
+	if err != nil {
+		t.Errorf("ALLOW error: %s", err)
+	}
 
+	// create a server socket
+	server := NewSock(PULL)
+	defer server.Destroy()
+
+	// set plain authentication
+	server.SetPlainServer(1)
+
+	// bind to an ephemeral port
 	port, err := server.Bind("tcp://127.0.0.1:*")
 	if port <= 0 {
 		t.Error("port should be > 0, is %d", port)
 	}
 
+	// create a client
+	client := NewSock(PUSH)
+	defer client.Destroy()
+
+	// set a username and password
+	client.SetPlainUsername("admin")
+	client.SetPlainPassword("bogus")
+
+	// try to connect
 	err = client.Connect(fmt.Sprintf("tcp://127.0.0.1:%d", port))
 	if err != nil {
 		t.Errorf("client connect error: %s", err)
 	}
 
+	// try to send a message.  since we have not told the auth
+	// service where a password file is, this should fail
 	client.SendString("Hello, World", 0)
 
 	poller, err := NewPoller(server)
 	if err != nil {
 		t.Errorf("NwPoller failed: %s", err)
 	}
-
 	defer poller.Destroy()
 
+	// poll for a message.  there should not be one.
 	s := poller.Wait(200)
 	if s != nil {
 		t.Error("poller received message should not have!")
+	}
+}
+
+func TestAuthPlainAllow(t *testing.T) {
+	pwfile, err := os.Create("./password_test.txt")
+	if err != nil {
+		t.Fatalf("could not create password test file")
+	}
+	defer func() {
+		os.Remove("./password_test.txt")
+	}()
+
+	w := bufio.NewWriter(pwfile)
+	w.Write([]byte("admin=Password\n"))
+	w.Flush()
+	pwfile.Close()
+
+	// create an auth service
+	auth := NewAuth()
+	defer auth.Destroy()
+
+	// set verbose
+	err = auth.Verbose()
+	if err != nil {
+		t.Errorf("VERBOSE error: %s", err)
+	}
+
+	// allow localhost connections
+	err = auth.Allow("127.0.0.1")
+	if err != nil {
+		t.Errorf("ALLOW error: %s", err)
+	}
+
+	err = auth.Plain("./password_test.txt")
+	if err != nil {
+		t.Errorf("PLAIN error: %s", err)
+	}
+
+	// create a server socket
+	server := NewSock(PULL)
+	defer server.Destroy()
+
+	// set plain authentication
+	server.SetPlainServer(1)
+
+	// bind to an ephemeral port
+	port, err := server.Bind("tcp://127.0.0.1:*")
+	if port <= 0 {
+		t.Error("port should be > 0, is %d", port)
+	}
+
+	// create a client
+	client := NewSock(PUSH)
+	defer client.Destroy()
+
+	// set a username and password
+	client.SetPlainUsername("admin")
+	client.SetPlainPassword("Password")
+
+	// try to connect
+	err = client.Connect(fmt.Sprintf("tcp://127.0.0.1:%d", port))
+	if err != nil {
+		t.Errorf("client connect error: %s", err)
+	}
+
+	// try to send a message.  since we have not told the auth
+	// service where a password file is, this should fail
+	client.SendString("Hello, World", 0)
+
+	poller, err := NewPoller(server)
+	if err != nil {
+		t.Errorf("NwPoller failed: %s", err)
+	}
+	defer poller.Destroy()
+
+	// poll for a message.  there should not be one.
+	s := poller.Wait(200)
+	if s == nil {
+		t.Error("poller should have waiting message!")
 	}
 }
