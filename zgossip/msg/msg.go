0fbcaf786ad2395db274e3570ca13130a4b812b9
Brian Knox <bknox@digitalocean.com>
2014-10-28 21:24:48 -0400

    adding zgossip package

---
+++ zgossip/msg/msg.go
@@ -0,0 +1,225 @@
+// Package msg is 100% generated. If you edit this file,
+// you will lose your changes at the next build cycle.
+// DO NOT MAKE ANY CHANGES YOU WISH TO KEEP.
+//
+// The correct places for commits are:
+//  - The XML model used for this code generation: zgossip_msg.xml
+//  - The code generation script that built this file: zproto_codec_goczmq
+package msg
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+
+	"github.com/taotetek/goczmq"
+)
+
+const (
+	Signature uint16 = 0xAAA0 | 0
+)
+
+const (
+	HelloId   uint8 = 1
+	PublishId uint8 = 2
+	PingId    uint8 = 3
+	PongId    uint8 = 4
+	InvalidId uint8 = 5
+)
+
+type Transit interface {
+	Marshal() ([]byte, error)
+	Unmarshal(...[]byte) error
+	String() string
+	Send(*goczmq.Sock) error
+	SetRoutingId([]byte)
+	RoutingId() []byte
+	SetVersion(byte)
+	Version() byte
+}
+
+// Unmarshals data from raw frames.
+func Unmarshal(frames ...[]byte) (t Transit, err error) {
+	if frames == nil {
+		return nil, errors.New("can't unmarshal an empty message")
+	}
+	var buffer *bytes.Buffer
+
+	// Check the signature
+	var signature uint16
+	buffer = bytes.NewBuffer(frames[0])
+	binary.Read(buffer, binary.BigEndian, &signature)
+	if signature != Signature {
+		// Invalid signature
+		return nil, errors.New("invalid signature")
+	}
+
+	// Get message id and parse per message type
+	var id uint8
+	binary.Read(buffer, binary.BigEndian, &id)
+
+	switch id {
+	case HelloId:
+		t = NewHello()
+	case PublishId:
+		t = NewPublish()
+	case PingId:
+		t = NewPing()
+	case PongId:
+		t = NewPong()
+	case InvalidId:
+		t = NewInvalid()
+	}
+	err = t.Unmarshal(frames...)
+
+	return t, err
+}
+
+// Receives marshaled data from 0mq socket.
+func Recv(socket *goczmq.Sock) (t Transit, err error) {
+	return recv(socket, 0)
+}
+
+// Receives marshaled data from 0mq socket. It won't wait for input.
+func RecvNoWait(socket *goczmq.Sock) (t Transit, err error) {
+	return recv(socket, goczmq.DONTWAIT)
+}
+
+// Receives marshaled data from 0mq socket.
+func recv(socket *goczmq.Sock, flag goczmq.Flag) (t Transit, err error) {
+	var frames [][]byte
+
+	if flag == goczmq.DONTWAIT {
+		frames, err = socket.RecvMessageNoWait()
+	} else {
+		frames, err = socket.RecvMessage()
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	sType := socket.GetType()
+	if err != nil {
+		return nil, err
+	}
+
+	var routingId []byte
+	// If message came from a router socket, first frame is routingId
+	if sType == goczmq.ROUTER {
+		if len(frames) <= 1 {
+			return nil, errors.New("no routingId")
+		}
+		routingId = frames[0]
+		frames = frames[1:]
+	}
+
+	t, err = Unmarshal(frames...)
+	if err != nil {
+		return nil, err
+	}
+
+	if sType == goczmq.ROUTER {
+		t.SetRoutingId(routingId)
+	}
+	return t, err
+}
+
+// Clones a message.
+func Clone(t Transit) Transit {
+
+	switch msg := t.(type) {
+	case *Hello:
+		cloned := NewHello()
+		routingId := make([]byte, len(msg.RoutingId()))
+		copy(routingId, msg.RoutingId())
+		cloned.SetRoutingId(routingId)
+		cloned.version = msg.version
+		return cloned
+
+	case *Publish:
+		cloned := NewPublish()
+		routingId := make([]byte, len(msg.RoutingId()))
+		copy(routingId, msg.RoutingId())
+		cloned.SetRoutingId(routingId)
+		cloned.version = msg.version
+		cloned.Key = msg.Key
+		cloned.Value = msg.Value
+		cloned.Ttl = msg.Ttl
+		return cloned
+
+	case *Ping:
+		cloned := NewPing()
+		routingId := make([]byte, len(msg.RoutingId()))
+		copy(routingId, msg.RoutingId())
+		cloned.SetRoutingId(routingId)
+		cloned.version = msg.version
+		return cloned
+
+	case *Pong:
+		cloned := NewPong()
+		routingId := make([]byte, len(msg.RoutingId()))
+		copy(routingId, msg.RoutingId())
+		cloned.SetRoutingId(routingId)
+		cloned.version = msg.version
+		return cloned
+
+	case *Invalid:
+		cloned := NewInvalid()
+		routingId := make([]byte, len(msg.RoutingId()))
+		copy(routingId, msg.RoutingId())
+		cloned.SetRoutingId(routingId)
+		cloned.version = msg.version
+		return cloned
+	}
+
+	return nil
+}
+
+// putString marshals a string into the buffer.
+func putString(buffer *bytes.Buffer, str string) {
+	size := len(str)
+	binary.Write(buffer, binary.BigEndian, byte(size))
+	binary.Write(buffer, binary.BigEndian, []byte(str[0:size]))
+}
+
+// getString unmarshals a string from the buffer.
+func getString(buffer *bytes.Buffer) string {
+	var size byte
+	binary.Read(buffer, binary.BigEndian, &size)
+	str := make([]byte, size)
+	binary.Read(buffer, binary.BigEndian, &str)
+	return string(str)
+}
+
+// putLongString marshals a string into the buffer.
+func putLongString(buffer *bytes.Buffer, str string) {
+	size := len(str)
+	binary.Write(buffer, binary.BigEndian, uint32(size))
+	binary.Write(buffer, binary.BigEndian, []byte(str[0:size]))
+}
+
+// getLongString unmarshals a string from the buffer.
+func getLongString(buffer *bytes.Buffer) string {
+	var size uint32
+	binary.Read(buffer, binary.BigEndian, &size)
+	str := make([]byte, size)
+	binary.Read(buffer, binary.BigEndian, &str)
+	return string(str)
+}
+
+// putBytes marshals []byte into the buffer.
+func putBytes(buffer *bytes.Buffer, data []byte) {
+	size := uint64(len(data))
+	binary.Write(buffer, binary.BigEndian, size)
+	binary.Write(buffer, binary.BigEndian, data)
+}
+
+// getBytes unmarshals []byte from the buffer.
+func getBytes(buffer *bytes.Buffer) []byte {
+	var size uint64
+	binary.Read(buffer, binary.BigEndian, &size)
+	data := make([]byte, size)
+	binary.Read(buffer, binary.BigEndian, &data)
+	return data
+}
---
+++ zgossip/msg/publish.go
@@ -0,0 +1,168 @@
+package msg
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/taotetek/goczmq"
+)
+
+// Client or server announces a new tuple
+type Publish struct {
+	routingId []byte
+	version   byte
+	Key       string
+	Value     string
+	Ttl       uint32
+}
+
+// New creates new Publish message.
+func NewPublish() *Publish {
+	publish := &Publish{}
+	return publish
+}
+
+// String returns print friendly name.
+func (p *Publish) String() string {
+	str := "ZGOSSIP_MSG_PUBLISH:\n"
+	str += fmt.Sprintf("    version = %v\n", p.version)
+	str += fmt.Sprintf("    Key = %v\n", p.Key)
+	str += fmt.Sprintf("    Value = %v\n", p.Value)
+	str += fmt.Sprintf("    Ttl = %v\n", p.Ttl)
+	return str
+}
+
+// Marshal serializes the message.
+func (p *Publish) Marshal() ([]byte, error) {
+	// Calculate size of serialized data
+	bufferSize := 2 + 1 // Signature and message ID
+
+	// version is a 1-byte integer
+	bufferSize += 1
+
+	// Key is a string with 1-byte length
+	bufferSize++ // Size is one byte
+	bufferSize += len(p.Key)
+
+	// Value is a string with 4-byte length
+	bufferSize += 4 // Size is 4 bytes
+	bufferSize += len(p.Value)
+
+	// Ttl is a 4-byte integer
+	bufferSize += 4
+
+	// Now serialize the message
+	tmpBuf := make([]byte, bufferSize)
+	tmpBuf = tmpBuf[:0]
+	buffer := bytes.NewBuffer(tmpBuf)
+	binary.Write(buffer, binary.BigEndian, Signature)
+	binary.Write(buffer, binary.BigEndian, PublishId)
+
+	// version
+	value, _ := strconv.ParseUint("1", 10, 1*8)
+	binary.Write(buffer, binary.BigEndian, byte(value))
+
+	// Key
+	putString(buffer, p.Key)
+
+	// Value
+	putLongString(buffer, p.Value)
+
+	// Ttl
+	binary.Write(buffer, binary.BigEndian, p.Ttl)
+
+	return buffer.Bytes(), nil
+}
+
+// Unmarshals the message.
+func (p *Publish) Unmarshal(frames ...[]byte) error {
+	if frames == nil {
+		return errors.New("Can't unmarshal empty message")
+	}
+
+	frame := frames[0]
+	frames = frames[1:]
+
+	buffer := bytes.NewBuffer(frame)
+
+	// Get and check protocol signature
+	var signature uint16
+	binary.Read(buffer, binary.BigEndian, &signature)
+	if signature != Signature {
+		return errors.New("invalid signature")
+	}
+
+	// Get message id and parse per message type
+	var id uint8
+	binary.Read(buffer, binary.BigEndian, &id)
+	if id != PublishId {
+		return errors.New("malformed Publish message")
+	}
+	// version
+	binary.Read(buffer, binary.BigEndian, &p.version)
+	if p.version != 1 {
+		return errors.New("malformed version message")
+	}
+	// Key
+	p.Key = getString(buffer)
+	// Value
+	p.Value = getLongString(buffer)
+	// Ttl
+	binary.Read(buffer, binary.BigEndian, &p.Ttl)
+
+	return nil
+}
+
+// Sends marshaled data through 0mq socket.
+func (p *Publish) Send(socket *goczmq.Sock) (err error) {
+	frame, err := p.Marshal()
+	if err != nil {
+		return err
+	}
+
+	socType := socket.GetType()
+	if err != nil {
+		return err
+	}
+
+	// If we're sending to a ROUTER, we send the routingId first
+	if socType == goczmq.ROUTER {
+		err = socket.SendBytes(p.routingId, goczmq.MORE)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Now send the data frame
+	err = socket.SendBytes(frame, 0)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+// RoutingId returns the routingId for this message, routingId should be set
+// whenever talking to a ROUTER.
+func (p *Publish) RoutingId() []byte {
+	return p.routingId
+}
+
+// SetRoutingId sets the routingId for this message, routingId should be set
+// whenever talking to a ROUTER.
+func (p *Publish) SetRoutingId(routingId []byte) {
+	p.routingId = routingId
+}
+
+// Setversion sets the version.
+func (p *Publish) SetVersion(version byte) {
+	p.version = version
+}
+
+// version returns the version.
+func (p *Publish) Version() byte {
+	return p.version
+}
