919d83b17873db10dc7a22054777e2f2a018f37b
Indradhanush Gupta <dhanush@instamojo.com>
2014-10-31 22:11:10 +0530

    Added weather-update client and server examples.

---
+++ examples/wuclient.go
@@ -0,0 +1,52 @@
+//
+// Weather proxy listens to weather server which is constantly
+// emitting weather data
+// Binds SUB socket to tcp://127.0.0.1:5556
+//
+// Usage : go run examples/wuclient.go 87654
+
+package main
+
+import (
+	"fmt"
+	czmq "github.com/zeromq/goczmq"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	pubEndpoint := "tcp://127.0.0.1:5556"
+	totalTemperature := 0
+
+	filter := "59937"
+	if len(os.Args) > 1 {
+		filter = string(os.Args[1])
+	}
+
+	subSock, err := czmq.NewSUB(pubEndpoint, filter)
+	if err != nil {
+		panic(err)
+	}
+
+	defer subSock.Destroy()
+
+	fmt.Printf("Collecting updates from weather server for %s…\n", filter)
+	subSock.Connect(pubEndpoint)
+
+	for i := 0; i < 100; i++ {
+		msg, err := subSock.RecvString()
+		if err != nil {
+			panic(err)
+		}
+
+		weatherData := strings.Split(msg, " ")
+		temperature, err := strconv.ParseInt(weatherData[1], 10, 64)
+		if err == nil {
+			totalTemperature += int(temperature)
+		}
+	}
+
+	fmt.Printf("Average temperature for zipcode %s was %dF \n\n", filter,
+		totalTemperature/100)
+}
