721c70e87132068c324c12a55de9a7c8041a0499
Ben Aldrich <vrecan@gmail.com>
2015-06-14 00:28:57 -0600

    working compile for windows

--- goczmq.go
+++ goczmq.go
@@ -6,7 +6,10 @@
 package goczmq
 
 /*
-#cgo pkg-config: libczmq libzmq libsodium
+#cgo !windows pkg-config: libczmq libzmq libsodium
+#cgo windows LDFLAGS: -lws2_32 -liphlpapi -lrpcrt4 -lsodium -lzmq -lczmq
+#cgo windows CFLAGS: -Wno-pedantic-ms-format -DLIBCZMQ_EXPORTS -DZMQ_DEFINED_STDINT -DLIBCZMQ_EXPORTS
+
 #include "czmq.h"
 #include <stdlib.h>
 #include <string.h>
